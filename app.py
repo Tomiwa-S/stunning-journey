2416f450773280b226056a7af904d28bde51c5ae
Tomiwa-S <timmeysam@gmail.com>
2025-09-20 08:33:39 +0100

    new page

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
-app = Flask(__name__)
 
-@app.route("/")
+web = Flask(__name__)
+
+@web.route("/")
 def home():
     return render_template("index.html")
 
+@web.route("/random")
+def random():
+    return "Some random text"
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    web.run(host="0.0.0.0", port=8080)
